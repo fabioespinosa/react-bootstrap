bc81edaad42c8b1886bb176a50f46c04243e12ee
Fabio Espinosa <fa.espinosa10@uniandes.edu.co>
2025-07-10 18:15:54 +0200

    fix

--- es/Fade.js
+++ es/Fade.js
@@ -1,93 +1,44 @@
 import _extends from "@babel/runtime-corejs3/helpers/esm/extends";
 import _objectWithoutPropertiesLoose from "@babel/runtime-corejs3/helpers/esm/objectWithoutPropertiesLoose";
-var _excluded = ["className", "children", "onEnter", "childRef"];
+var _excluded = ["className", "children", "transitionClasses", "onEnter"];
 var _fadeStyles;
 import classNames from 'classnames';
-import React, { useCallback, useMemo, useRef } from 'react';
-import PropTypes from 'prop-types';
-import Transition, { ENTERED, ENTERING } from 'react-transition-group/Transition';
-function triggerBrowserReflow(node) {
-  // eslint-disable-next-line @typescript-eslint/no-unused-expressions
-  node.offsetHeight;
-}
-var propTypes = {
-  /**
-   * Show the component; triggers the fade in or fade out animation
-   */
-  "in": PropTypes.bool,
-  /**
-   * Wait until the first "enter" transition to mount the component (add it to the DOM)
-   */
-  mountOnEnter: PropTypes.bool,
-  /**
-   * Unmount the component (remove it from the DOM) when it is faded out
-   */
-  unmountOnExit: PropTypes.bool,
-  /**
-   * Run the fade in animation when the component mounts, if it is initially
-   * shown
-   */
-  appear: PropTypes.bool,
-  /**
-   * Duration of the fade animation in milliseconds, to ensure that finishing
-   * callbacks are fired even if the original browser transition end events are
-   * canceled
-   */
-  timeout: PropTypes.number,
-  /**
-   * Callback fired before the component fades in
-   */
-  onEnter: PropTypes.func,
-  /**
-   * Callback fired after the component starts to fade in
-   */
-  onEntering: PropTypes.func,
-  /**
-   * Callback fired after the has component faded in
-   */
-  onEntered: PropTypes.func,
-  /**
-   * Callback fired before the component fades out
-   */
-  onExit: PropTypes.func,
-  /**
-   * Callback fired after the component starts to fade out
-   */
-  onExiting: PropTypes.func,
-  /**
-   * Callback fired after the component has faded out
-   */
-  onExited: PropTypes.func
-};
-var defaultProps = {
-  "in": false,
-  timeout: 300,
-  mountOnEnter: false,
-  unmountOnExit: false,
-  appear: false
-};
+import React, { useCallback } from 'react';
+import { ENTERED, ENTERING } from 'react-transition-group/Transition';
+import { getChildRef } from '@restart/ui/utils';
+import transitionEndListener from './transitionEndListener';
+import triggerBrowserReflow from './triggerBrowserReflow';
+import TransitionWrapper from './TransitionWrapper';
 var fadeStyles = (_fadeStyles = {}, _fadeStyles[ENTERING] = 'in', _fadeStyles[ENTERED] = 'in', _fadeStyles);
-var Fade = /*#__PURE__*/React.forwardRef(function (props, ref) {
-  var className = props.className,
-    children = props.children,
-    onEnter = props.onEnter,
-    childRef = props.childRef,
-    rest = _objectWithoutPropertiesLoose(props, _excluded);
+var Fade = /*#__PURE__*/React.forwardRef(function (_ref, ref) {
+  var className = _ref.className,
+    children = _ref.children,
+    _ref$transitionClasse = _ref.transitionClasses,
+    transitionClasses = _ref$transitionClasse === void 0 ? {} : _ref$transitionClasse,
+    onEnter = _ref.onEnter,
+    rest = _objectWithoutPropertiesLoose(_ref, _excluded);
+  var props = _extends({
+    "in": false,
+    timeout: 300,
+    mountOnEnter: false,
+    unmountOnExit: false,
+    appear: false
+  }, rest);
   var handleEnter = useCallback(function (node, isAppearing) {
     triggerBrowserReflow(node);
     onEnter == null || onEnter(node, isAppearing);
   }, [onEnter]);
-  var nodeRef = useRef(null);
-  return /*#__PURE__*/React.createElement(Transition, _extends({}, rest, {
+  return /*#__PURE__*/React.createElement(TransitionWrapper, _extends({
     ref: ref,
-    nodeRef: nodeRef,
-    onEnter: handleEnter
+    addEndListener: transitionEndListener
+  }, props, {
+    onEnter: handleEnter,
+    childRef: getChildRef(children)
   }), function (status, innerProps) {
     return /*#__PURE__*/React.cloneElement(children, _extends({}, innerProps, {
-      ref: nodeRef,
-      className: classNames('fade', className, children.props.className, fadeStyles[status])
+      className: classNames('fade', className, children.props.className, fadeStyles[status], transitionClasses[status])
     }));
   });
 });
-Fade.defaultProps = defaultProps;
+Fade.displayName = 'Fade';
 export default Fade;
--- src/Fade.js
+++ src/Fade.js
@@ -1,113 +1,60 @@
 import classNames from 'classnames';
-import React, { useCallback, useMemo, useRef } from 'react';
-import PropTypes from 'prop-types';
-import Transition, {
-  ENTERED,
-  ENTERING
-} from 'react-transition-group/Transition';
-
-function triggerBrowserReflow(node) {
-  // eslint-disable-next-line @typescript-eslint/no-unused-expressions
-  node.offsetHeight;
-}
-
-const propTypes = {
-  /**
-   * Show the component; triggers the fade in or fade out animation
-   */
-  in: PropTypes.bool,
-
-  /**
-   * Wait until the first "enter" transition to mount the component (add it to the DOM)
-   */
-  mountOnEnter: PropTypes.bool,
-
-  /**
-   * Unmount the component (remove it from the DOM) when it is faded out
-   */
-  unmountOnExit: PropTypes.bool,
-
-  /**
-   * Run the fade in animation when the component mounts, if it is initially
-   * shown
-   */
-  appear: PropTypes.bool,
-
-  /**
-   * Duration of the fade animation in milliseconds, to ensure that finishing
-   * callbacks are fired even if the original browser transition end events are
-   * canceled
-   */
-  timeout: PropTypes.number,
-
-  /**
-   * Callback fired before the component fades in
-   */
-  onEnter: PropTypes.func,
-  /**
-   * Callback fired after the component starts to fade in
-   */
-  onEntering: PropTypes.func,
-  /**
-   * Callback fired after the has component faded in
-   */
-  onEntered: PropTypes.func,
-  /**
-   * Callback fired before the component fades out
-   */
-  onExit: PropTypes.func,
-  /**
-   * Callback fired after the component starts to fade out
-   */
-  onExiting: PropTypes.func,
-  /**
-   * Callback fired after the component has faded out
-   */
-  onExited: PropTypes.func
-};
-
-const defaultProps = {
-  in: false,
-  timeout: 300,
-  mountOnEnter: false,
-  unmountOnExit: false,
-  appear: false
-};
+import React, { useCallback } from 'react';
+import { ENTERED, ENTERING } from 'react-transition-group/Transition';
+import { getChildRef } from '@restart/ui/utils';
+import transitionEndListener from './transitionEndListener';
+import triggerBrowserReflow from './triggerBrowserReflow';
+import TransitionWrapper from './TransitionWrapper';
 
 const fadeStyles = {
   [ENTERING]: 'in',
   [ENTERED]: 'in'
 };
 
-const Fade = React.forwardRef((props, ref) => {
-  const { className, children, onEnter, childRef, ...rest } = props;
-  const handleEnter = useCallback(
-    (node, isAppearing) => {
-      triggerBrowserReflow(node);
-      onEnter?.(node, isAppearing);
-    },
-    [onEnter]
-  );
-  const nodeRef = useRef(null);
+const Fade = React.forwardRef(
+  ({ className, children, transitionClasses = {}, onEnter, ...rest }, ref) => {
+    const props = {
+      in: false,
+      timeout: 300,
+      mountOnEnter: false,
+      unmountOnExit: false,
+      appear: false,
+      ...rest
+    };
 
-  return (
-    <Transition {...rest} ref={ref} nodeRef={nodeRef} onEnter={handleEnter}>
-      {(status, innerProps) =>
-        React.cloneElement(children, {
-          ...innerProps,
-          ref: nodeRef,
-          className: classNames(
-            'fade',
-            className,
-            children.props.className,
-            fadeStyles[status]
-          )
-        })
-      }
-    </Transition>
-  );
-});
+    const handleEnter = useCallback(
+      (node, isAppearing) => {
+        triggerBrowserReflow(node);
+        onEnter?.(node, isAppearing);
+      },
+      [onEnter]
+    );
 
-Fade.defaultProps = defaultProps;
+    return (
+      <TransitionWrapper
+        ref={ref}
+        addEndListener={transitionEndListener}
+        {...props}
+        onEnter={handleEnter}
+        childRef={getChildRef(children)}
+      >
+        {(status, innerProps) =>
+          React.cloneElement(children, {
+            ...innerProps,
+            className: classNames(
+              'fade',
+              className,
+              children.props.className,
+              fadeStyles[status],
+              transitionClasses[status]
+            )
+          })
+        }
+      </TransitionWrapper>
+    );
+  }
+);
+
+Fade.displayName = 'Fade';
 
 export default Fade;
