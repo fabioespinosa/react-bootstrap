ba1e8e418534ed2a552997fd2f5fe4c33149c258
Fabio Espinosa <fa.espinosa10@uniandes.edu.co>
2025-07-10 20:11:25 +0200

    fix: missing index

---
+++ cjs/index.js
@@ -0,0 +1,23 @@
+"use strict";
+
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
+exports.__esModule = true;
+exports.ThemeProvider = exports.ModalTitle = exports.ModalHeader = exports.ModalFooter = exports.ModalDialog = exports.ModalBody = exports.Modal = exports.Fade = exports.CloseButton = void 0;
+var _CloseButton = _interopRequireDefault(require("./CloseButton"));
+exports.CloseButton = _CloseButton.default;
+var _Fade = _interopRequireDefault(require("./Fade"));
+exports.Fade = _Fade.default;
+var _Modal = _interopRequireDefault(require("./Modal"));
+exports.Modal = _Modal.default;
+var _ModalBody = _interopRequireDefault(require("./ModalBody"));
+exports.ModalBody = _ModalBody.default;
+var _ModalDialog = _interopRequireDefault(require("./ModalDialog"));
+exports.ModalDialog = _ModalDialog.default;
+var _ModalFooter = _interopRequireDefault(require("./ModalFooter"));
+exports.ModalFooter = _ModalFooter.default;
+var _ModalHeader = _interopRequireDefault(require("./ModalHeader"));
+exports.ModalHeader = _ModalHeader.default;
+var _ModalTitle = _interopRequireDefault(require("./ModalTitle"));
+exports.ModalTitle = _ModalTitle.default;
+var _ThemeProvider = _interopRequireDefault(require("./ThemeProvider"));
+exports.ThemeProvider = _ThemeProvider.default;
---
+++ src/index.tsx
@@ -0,0 +1,25 @@
+export { default as CloseButton } from './CloseButton';
+export type { CloseButtonProps } from './CloseButton';
+
+export { default as Fade } from './Fade';
+export type { FadeProps } from './Fade';
+
+export { default as Modal } from './Modal';
+export type { ModalProps } from './Modal';
+
+export { default as ModalBody } from './ModalBody';
+
+export { default as ModalDialog } from './ModalDialog';
+export type { ModalDialogProps } from './ModalDialog';
+
+export { default as ModalFooter } from './ModalFooter';
+export type { ModalFooterProps } from './ModalFooter';
+
+export { default as ModalHeader } from './ModalHeader';
+export type { ModalHeaderProps } from './ModalHeader';
+
+export { default as ModalTitle } from './ModalTitle';
+export type { ModalTitleProps } from './ModalTitle';
+
+export { default as ThemeProvider } from './ThemeProvider';
+export type { ThemeProviderProps } from './ThemeProvider';
